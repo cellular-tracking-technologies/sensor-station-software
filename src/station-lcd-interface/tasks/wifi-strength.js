31a05509ce8be9071aa9199a4075ea225d54b75e
Meelyn Pandit <meelyn.pandit@celltracktech.com>
2024-02-27 13:58:58 -0500

    wifi strength menu option displays wifi bars in one block

--- src/station-lcd-interface/tasks/wifi-strength.js
+++ src/station-lcd-interface/tasks/wifi-strength.js
@@ -8,7 +8,7 @@ import { Buffer } from 'node:buffer'
 class WifiStrength extends WifiSignal {
   constructor() {
     super()
-    this.header = "WiFi Signal Strength"
+    this.header = "WiFi:"
     this.cmd = 'iwconfig | grep "Link Quality"'
     this.display = display
     // this.lcd = new LCD()
@@ -23,41 +23,33 @@ class WifiStrength extends WifiSignal {
       const percent = await this.getWifiSignal()
       resolve(percent)
 
-    }).then((values) => {
+    }).then(async (values) => {
       console.log('wifi strength values', values)
       let rows = [this.header]
-      let bar0, bar1, bar2
-      if (values > 50) {
+      let bars
+      if (values > 75) {
 
         // bar0 = String.fromCharCode(0xff, 0xff, 0xff, 0xff, 0xff)
-        bar0 = Buffer.from([0x1f, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x1f], 'hex')
-        let arrByte = Uint8Array.from(bar0)
+        bars = Buffer.from([0x00, 0x00, 0x00, 0x01, 0x03, 0x07, 0x0f, 0x1f], 'hex')
+        await this.createChar(bars)
+      } else if (values <= 75 && values > 50) {
+        bars = Buffer.from([0x00, 0x00, 0x00, 0x00, 0x02, 0x06, 0x0e, 0x1e], 'hex')
+        await this.createChar(bars)
 
-        this.display.lcd.createChar(0, arrByte)
-        this.display.lcd.print('\x00')
-        // this.display.lcd.setCursor(1, 0)
-        // this.display.lcd.printlnBlock('hello world', 1)
-        // this.display.writeRow('\x00', 2)
-        // console.log('custom character', custom_char)
-        // console.log('bar0 byte array', String.fromCharCode(custom_char))
-        // rows.push(custom_char)
+        console.log('med strength', rows)
+      } else if (values <= 50 && values > 25) {
+        bars = Buffer.from([0x00, 0x00, 0x00, 0x00, 0x02, 0x04, 0x0c, 0x1c], 'hex')
+        await this.createChar(bars)
 
-        console.log('high strength', rows)
-        // } else if (values <= 75 && values > 50) {
-        //   bar0 = Buffer.from([0x1f, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x1f], 'hex')
-        //   console.log('bar0 data', bar0.toString())
-        //   let arrByte = Uint8Array.from(bar0)
-        //   console.log('bar0 byte array', String.fromCharCode(arrByte))
-        //   rows.push('\u0001')
-
-        //   console.log('med strength', rows)
-        // } else if (values <= 50 && values > 25) {
-        //   bar0 = String.fromCharCode(0xff, 0xff, 0xff, 0xff, 0xff)
-        //   rows.push(bar0)
-        //   console.log('low strength', rows)
-        // } else if (values <= 25 && values > 0) {
-        //   bars = 0x10
-        //   console.log('no strength', rows)
+        // rows.push(bar0)
+        console.log('low strength', rows)
+      } else if (values <= 25 && values > 0) {
+        bars = Buffer.from([0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x08, 0x18], 'hex')
+        await this.createChar(bars)
+        console.log('no strength', rows)
+      } else {
+        bars = Buffer.from([0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x10], 'hex')
+        await this.createChar(bars)
       }
 
       console.log('rows', rows)
@@ -65,6 +57,13 @@ class WifiStrength extends WifiSignal {
     }).catch((e) => {
       console.error(e)
     })
+  }
+
+  async createChar(bars) {
+    let arrByte = Uint8Array.from(bars)
+    this.display.lcd.createChar(0, arrByte)
+    this.display.lcd.setCursor(6, 0)
+    this.display.lcd.print('\x00')
   }
 }
 
