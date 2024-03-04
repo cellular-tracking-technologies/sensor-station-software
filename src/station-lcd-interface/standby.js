e1877cf2b753249dd4d95c19c49d0ed240be5682
Meelyn Pandit <meelyn.pandit@celltracktech.com>
2024-03-04 15:35:57 -0500

    created additional get functions and added degree symbol to temperature values

--- src/station-lcd-interface/standby.js
+++ src/station-lcd-interface/standby.js
@@ -10,79 +10,23 @@ import display from './display-driver.js'
  */
 class StandBy {
   /**
-   * 
+   * @param {String} host ip address of server
    */
   constructor(host) {
+    console.log('stand by host', host)
     this.wifi = new WifiStrength(host)
     this.power = new SensorVoltageTask(host)
     this.temp = new SensorTemperatureTask(host)
-    // this.location = new Location()
     this.cellular = new CellularCarrier(host)
-    // this.cellular_id = new CellularIds(host)
-    this.gps = new GpsTask(host)
   }
 
   clearScreen() {
     display.clear()
   }
 
-  async getBattVoltage() {
-    let battery_results = await this.power.results()
-    console.log('battery results', battery_results)
-    let volt = Number(battery_results[1].match(/(\d.+)/g))
-    await this.createBattChar(volt)
-  }
-
-  async createBattChar(voltage) {
-    console.log('voltage', voltage)
-    let bar0, bar1, bar2, top, arrByte
-    if (voltage > 11.65) {
-
-      display.lcd.setCursor(0, 2)
-      display.lcd.print(`\xff`)
-
-      display.lcd.setCursor(1, 2)
-      display.lcd.print(`\xff`)
-
-      display.lcd.setCursor(2, 2)
-      display.lcd.print(`\xff`)
-
-      top = Buffer.from([0x00, 0x00, 0x18, 0x18, 0x18, 0x18, 0x00, 0x00], 'hex')
-      arrByte = Uint8Array.from(top)
-      display.lcd.createChar(4, arrByte)
-      display.lcd.setCursor(3, 2)
-      display.lcd.print(`\x04`)
-
-    } else if (voltage <= 11.65 && voltage > 10) {
-      // bar0 = Buffer.from([0x1f, 0x1f, 0x1f, 0x1f, 0x1f, 0x1f, 0x1f, 0x1f], 'hex')
-      // arrByte = Uint8Array.from(bar0)
-      // display.lcd.createChar(1, arrByte)
-      // display.lcd.setCursor(0, 2)
-      // display.lcd.print(`\x01`)
-
-      // bar1 = Buffer.from([0x1f, 0x1f, 0x1f, 0x1f, 0x1f, 0x1f, 0x1f, 0x1f], 'hex')
-      // arrByte = Uint8Array.from(bar1)
-      // display.lcd.createChar(2, arrByte)
-      // display.lcd.setCursor(1, 2)
-      // display.lcd.print(`\x02`)
-      display.lcd.setCursor(0, 2)
-      display.lcd.print(`\xff`)
-
-      display.lcd.setCursor(1, 2)
-      display.lcd.print(`\xff`)
-
-      bar2 = Buffer.from([0x1f, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x1f], 'hex')
-      arrByte = Uint8Array.from(bar2)
-      display.lcd.createChar(3, arrByte)
-      display.lcd.setCursor(2, 2)
-      display.lcd.print(`\x03`)
-
-      top = Buffer.from([0x00, 0x00, 0x18, 0x18, 0x18, 0x18, 0x00, 0x00], 'hex')
-      arrByte = Uint8Array.from(top)
-      display.lcd.createChar(4, arrByte)
-      display.lcd.setCursor(3, 2)
-      display.lcd.print(`\x04`)
-    }
+  async getWifiStrength() {
+    let wifi_results = await this.wifi.results()
+    await this.createWifiChar(wifi_results)
   }
 
   async createWifiChar(percent) {
@@ -130,6 +74,64 @@ class StandBy {
     }
   }
 
+  async getBattVoltage() {
+    let battery_results = await this.power.results()
+    console.log('battery results', battery_results)
+    let volt = Number(battery_results[1].match(/(\d.+)/g))
+    await this.createBattChar(volt)
+  }
+
+  async createBattChar(voltage) {
+    console.log('voltage', voltage)
+    let bar0, bar1, bar2, top, arrByte
+    if (voltage > 11.65) {
+
+      display.lcd.setCursor(0, 2)
+      display.lcd.print(`\xff`)
+
+      display.lcd.setCursor(1, 2)
+      display.lcd.print(`\xff`)
+
+      display.lcd.setCursor(2, 2)
+      display.lcd.print(`\xff`)
+
+      top = Buffer.from([0x00, 0x00, 0x18, 0x18, 0x18, 0x18, 0x00, 0x00], 'hex')
+      arrByte = Uint8Array.from(top)
+      display.lcd.createChar(4, arrByte)
+      display.lcd.setCursor(3, 2)
+      display.lcd.print(`\x04`)
+
+    } else if (voltage <= 11.65 && voltage > 10) {
+
+      display.lcd.setCursor(0, 2)
+      display.lcd.print(`\xff`)
+
+      display.lcd.setCursor(1, 2)
+      display.lcd.print(`\xff`)
+
+      bar2 = Buffer.from([0x1f, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x1f], 'hex')
+      arrByte = Uint8Array.from(bar2)
+      display.lcd.createChar(3, arrByte)
+      display.lcd.setCursor(2, 2)
+      display.lcd.print(`\x03`)
+
+      top = Buffer.from([0x00, 0x00, 0x18, 0x18, 0x18, 0x18, 0x00, 0x00], 'hex')
+      arrByte = Uint8Array.from(top)
+      display.lcd.createChar(4, arrByte)
+      display.lcd.setCursor(3, 2)
+      display.lcd.print(`\x04`)
+    }
+  }
+
+  async getCellStrength() {
+    let cell_results = await this.cellular.results()
+    console.log('cell regex', cell_results[2].match(/(-)\w+/g))
+    let rssi = cell_results[2].match(/(-)\w+/g) ? Number(cell_results[2].match(/(-)\w+/g)) : undefined
+    console.log('cell results rssi', cell_results[2], rssi)
+    await this.createCellChar(rssi)
+    console.log('cellular', await this.cellular.results())
+  }
+
   async createCellChar(rssi) {
     let block_left, block_right, arrByte0, arrByte1
     if (rssi > -113) {
@@ -169,32 +171,26 @@ class StandBy {
     }
   }
 
-  async results() {
-    let wifi_results = await this.wifi.results()
-    console.log('wifi results', wifi_results)
-    await this.createWifiChar(wifi_results)
-
-    await this.getBattVoltage()
-
-    let cell_results = await this.cellular.results()
-    console.log('cell regex', cell_results[2].match(/(-)\w+/g))
-    let rssi = cell_results[2].match(/(-)\w+/g) ? Number(cell_results[2].match(/(-)\w+/g)) : undefined
-    console.log('cell results rssi', cell_results[2], rssi)
-    await this.createCellChar(rssi)
-    console.log('cellular', await this.cellular.results())
-
+  async getTempValues() {
     let temp_results = await this.temp.results()
     console.log('temp results', temp_results)
 
-    let regex_temp = temp_results[1].match(/(-\d)\w+/g)
+    let regex_temp = temp_results[1].match(/-?\d+/g)
     console.log('temp results regex', temp_results, regex_temp)
 
     display.lcd.setCursor(12, 0)
-    display.lcd.print(`${regex_temp[0]}`)
+    display.lcd.print(`${regex_temp[0]}${`\xdf`}C`)
 
     display.lcd.setCursor(12, 1)
-    display.lcd.print(`${regex_temp[1]}`)
+    display.lcd.print(`${regex_temp[1]}${`\xdf`}F`)
+  }
 
+  async results() {
+
+    await this.getWifiStrength()
+    await this.getBattVoltage()
+    await this.getCellStrength()
+    await this.getTempValues()
   }
 
 }
