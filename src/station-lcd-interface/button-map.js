716f55efd68814e87ef5e3ae8dfc0a1c5f413326
Meelyn Pandit <meelyn.pandit@celltracktech.com>
2024-03-18 16:21:17 -0400

    removed kernel conditionals from this branch for testing on v2

--- src/station-lcd-interface/button-map.js
+++ src/station-lcd-interface/button-map.js
@@ -1,45 +1,40 @@
 import Revision from '../revision.js'
-import { KernelVersion } from '../hardware/kernel/kernel.js'
-
-let kernel = new KernelVersion()
-let kernel_version = kernel.getVersion()
 
 let Buttons = {}
-if (kernel_version === '6.6.20') {
 
-	if (Revision.revision >= 3) {
-		Buttons = {
-			Up: 529,
-			Down: 534,
-			Select: 539,
-			Back: 520,
-		}
-	} else {
-		Buttons = {
-			Up: 516,
-			Down: 517,
-			Select: 518,
-			Back: 519,
-		}
+if (Revision.revision >= 3) {
+	Buttons = {
+		Up: 529,
+		Down: 534,
+		Select: 539,
+		Back: 520,
 	}
 } else {
-	if (Revision.revision >= 3) {
-		Buttons = {
-			Up: 17,
-			Down: 22,
-			Select: 27,
-			Back: 8
-
-		}
-	} else {
-		Buttons = {
-			Up: 4,
-			Down: 5,
-			Select: 6,
-			Back: 7
-
-		}
+	Buttons = {
+		Up: 516,
+		Down: 517,
+		Select: 518,
+		Back: 519,
 	}
 }
+
+// if (Revision.revision >= 3) {
+// 	Buttons = {
+// 		Up: 17,
+// 		Down: 22,
+// 		Select: 27,
+// 		Back: 8
+
+// 	}
+// } else {
+// 	Buttons = {
+// 		Up: 4,
+// 		Down: 5,
+// 		Select: 6,
+// 		Back: 7
+
+// 	}
+// }
+// }
 
 export default Buttons
