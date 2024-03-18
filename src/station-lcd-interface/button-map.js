d8fba116379e16e947797c61a35eea7221079212
Meelyn Pandit <meelyn.pandit@celltracktech.com>
2024-03-18 11:46:39 -0400

    conditionals for linux kernel

--- src/station-lcd-interface/button-map.js
+++ src/station-lcd-interface/button-map.js
@@ -1,28 +1,43 @@
 import Revision from '../revision.js'
+import os from 'os'
 
+let osVersion = os.version().match(/(?<=\:)\d.\d.{2,3}/g).toString()
+console.log(osVersion)
 let Buttons = {}
+if (osVersion === '6.6.20') {
 
-if (Revision.revision >= 3) {
-	Buttons = {
-		// Up: 17,
-		// Down: 22,
-		// Select: 27,
-		// Back: 8
-		Up: 529,
-		Down: 534,
-		Select: 539,
-		Back: 520,
+	if (Revision.revision >= 3) {
+		Buttons = {
+			Up: 529,
+			Down: 534,
+			Select: 539,
+			Back: 520,
+		}
+	} else {
+		Buttons = {
+			Up: 516,
+			Down: 517,
+			Select: 518,
+			Back: 519,
+		}
 	}
 } else {
-	Buttons = {
-		// Up: 4,
-		// Down: 5,
-		// Select: 6,
-		// Back: 7
-		Up: 516,
-		Down: 517,
-		Select: 518,
-		Back: 519,
+	if (Revision.revision >= 3) {
+		Buttons = {
+			Up: 17,
+			Down: 22,
+			Select: 27,
+			Back: 8
+
+		}
+	} else {
+		Buttons = {
+			Up: 4,
+			Down: 5,
+			Select: 6,
+			Back: 7
+
+		}
 	}
 }
 
