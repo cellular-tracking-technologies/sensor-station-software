45ad80b6fd7b22ea99b40e26b771655e91ab1955
Meelyn Pandit <meelyn.pandit@celltracktech.com>
2024-03-21 11:24:26 -0400

    changed v2 radio map again, from 1.2.2 to 1.2.3

--- system/radios/v2-blu-radio-map.js
+++ system/radios/v2-blu-radio-map.js
@@ -15,7 +15,7 @@
   "path": "/dev/serial/by-path/platform-3f980000.usb-usb-0:1.1:1.0-port0"
 }, {
   "channel": 6,
-  "path": "/dev/serial/by-path/platform-3f980000.usb-usb-0:1.2.2:1.0-port0"
+  "path": "/dev/serial/by-path/platform-3f980000.usb-usb-0:1.2.3:1.0-port0"
 }, {
   "channel": 7,
   "path": "/dev/serial/by-path/platform-3f980000.usb-usb-0:1.2.4:1.0-port0"
