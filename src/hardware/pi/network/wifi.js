cb1dc5a4fc086c9ac8199243e46d19f6a1b72b63
Bob Fogg <Bob Fogg>
2024-06-06 11:08:41 -0400

    update wifi utility to get networks / current network

--- src/hardware/pi/network/wifi.js
+++ src/hardware/pi/network/wifi.js
@@ -1,23 +1,36 @@
 import { execSync } from 'child_process'
 
+/**
+ * 
+ * @returns {Array}
+ */
 const GetNetworkList = () => {
-  const network_info = execSync('nmcli -t -f IN-USE,SSID,SIGNAL device wifi list').toString().trim()
+  const network_info = execSync('nmcli -t -f IN-USE,SSID,SIGNAL,RATE,FREQ device wifi list').toString().trim()
   return network_info.split('\n').map((info) => {
-    const [in_use, ssid, signal] = info.split(':')
+    const [in_use, ssid, signal, rate, freq] = info.split(':')
     return {
       connected: (in_use === '*') ? true : false,
       ssid,
-      signal,
+      signal: parseInt(signal),
+      rate,
+      freq
     }
   })
 }
 
 export default Object.freeze({
-  GetSignal: () => {
-    // return result
-    GetNetworkList().find(network => network.connected ? true : false)
+  /**
+   * 
+   * @returns {Object} return 
+   */
+  GetCurrentNetwork: () => {
+    return GetNetworkList().find(network => network.connected)
   },
-  ScanNetworks: () => {
+  /**
+   * 
+   * @returns {Array} array of networks visible to twifi
+   */
+  GetNetworks: () => {
     return GetNetworkList()
   }
 })
