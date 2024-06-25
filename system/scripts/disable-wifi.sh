a7abc51829744581da805298bc19f4a1637c16c2
Bob Fogg <Bob Fogg>
2024-06-25 15:51:06 -0400

    update wifi chipsets for bullseye

--- system/scripts/disable-wifi.sh
+++ system/scripts/disable-wifi.sh
@@ -1,14 +1,38 @@
 #!/bin/bash
 
-mt7601u_chip=$(lsmod | grep mt7601u | wc -l)
-rtw88_8821cu_chip=$(lsmod | grep rtw88_8821cu | wc -l)
+# load chipsets array
+source kernel-chipsets.sh
 
-if [ $mt7601u_chip -gt 0 ]; then 
-    echo 'blacklist mt7601u' > /etc/modprobe.d/blacklist-mt7601u.conf
-    modprobe -r mt7601u;
-fi
+disable_chip () 
+{
+    chipset=$1
+    filename="/etc/modprobe.d/blacklist-$chipset.conf"
+    echo "writing to $filename"
+    echo "blacklist $chipset" > $filename
+    modprobe -r "$chipset"
+}
 
-if [ $rtw88_8821cu_chip -gt 0 ]; then
-    echo 'blacklist rtw88_8821cu' > /etc/modprobe.d/blacklist-rtl8811cu.conf
-    modprobe -r rtw88_8821cu;
-fi
+handle_chip ()
+{
+    chipset=$1
+    if [ -z "$chipset" ]; then
+        echo "chipset expected as first argument"
+        return -1
+    fi
+
+    chip_check=$(lsmod | grep -a $chipset | wc -l)
+    if [ $chip_check -gt 0 ]; then
+        disable_chip $chipset
+        return 0
+    else
+        return -1
+    fi
+}
+
+chipcount=${#CHIPS[@]}
+for ((i=0; i< ${chipcount}; i++ ));
+do 
+    chipset=${CHIPS[$i]}
+    handle_chip $chipset
+done
+
