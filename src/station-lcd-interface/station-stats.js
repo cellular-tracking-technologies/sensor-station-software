fb3f4fcd84e9cf9909b859e0537b985b687ef55a
Meelyn Pandit <meelyn.pandit@gmail.com>
2024-08-19 19:30:58 +0000

    removed old functions

--- src/station-lcd-interface/station-stats.js
+++ src/station-lcd-interface/station-stats.js
@@ -132,109 +132,6 @@ class StationStats {
     }
   }
 
-  async createBattChar(voltage) {
-    // create top part of battery icon
-    display.lcd.createChar(battery.top.char, battery.top.byte)
-
-    // create empty bar of battery icon
-    display.lcd.createChar(battery.empty_bar.char, battery.empty_bar.byte)
-
-    // print top part of battery
-    display.lcd.setCursor(9, 0)
-    display.lcd.print(battery.top.hex)
-
-    if (voltage > thresholds.battery.max) {
-
-      // print bar0
-      display.lcd.setCursor(6, 0)
-      display.lcd.print(battery.full_bar.hex)
-
-      // print bar1
-      display.lcd.setCursor(7, 0)
-      display.lcd.print(battery.full_bar.hex)
-
-      // print bar2
-      display.lcd.setCursor(8, 0)
-      display.lcd.print(battery.full_bar.hex)
-
-    } else if (voltage <= thresholds.battery.max && voltage > thresholds.battery.min) {
-
-      // print bar0
-      display.lcd.setCursor(6, 0)
-      display.lcd.print(battery.full_bar.hex)
-
-      // print bar1
-      display.lcd.setCursor(7, 0)
-      display.lcd.print(battery.full_bar.hex)
-
-      // print bar2
-      display.lcd.setCursor(8, 0)
-      display.lcd.print(battery.empty_bar.hex)
-
-
-    } else if (voltage <= thresholds.battery.min) {
-
-      // print bar0
-      display.lcd.setCursor(6, 0)
-      display.lcd.print(battery.full_bar.hex)
-
-      // print bar1
-      display.lcd.setCursor(7, 0)
-      display.lcd.print(battery.empty_bar.hex)
-
-      // print bar2
-      display.lcd.setCursor(8, 0)
-      display.lcd.print(battery.empty_bar.hex)
-    } else {
-      display.lcd.setCursor(6, 0)
-      display.lcd.print(battery.warning.hex)
-    }
-  }
-
-  async getSolarVoltage(voltage) {
-    try {
-      await this.createSolarChar(Number(voltage.solar))
-    } catch (e) {
-      console.error('lcd solar error', e)
-      await this.createSolarChar(null)
-    }
-  }
-
-  async createSolarChar(voltage) {
-
-    if (voltage > thresholds.solar.max) {
-      display.lcd.createChar(solar.block_left.char, solar.block_left.byte.high)
-      display.lcd.setCursor(11, 0)
-      display.lcd.print(solar.block_left.hex)
-
-      display.lcd.createChar(solar.block_right.char, solar.block_right.byte.high)
-      display.lcd.setCursor(12, 0)
-      display.lcd.print(solar.block_right.hex)
-
-    } else if (voltage <= thresholds.solar.max && voltage > thresholds.solar.min) {
-      display.lcd.createChar(solar.block_left.char, solar.block_left.byte.med)
-      display.lcd.setCursor(11, 0)
-      display.lcd.print(solar.block_left.hex)
-
-      display.lcd.createChar(solar.block_right.char, solar.block_right.byte.med)
-      display.lcd.setCursor(12, 0)
-      display.lcd.print(solar.block_right.hex)
-
-    } else if (voltage <= thresholds.solar.min) {
-      display.lcd.createChar(solar.block_left.char, solar.block_left.byte.low)
-      display.lcd.setCursor(11, 0)
-      display.lcd.print(solar.block_left.hex)
-
-      display.lcd.createChar(solar.block_right.char, solar.block_right.byte.low)
-      display.lcd.setCursor(12, 0)
-      display.lcd.print(solar.block_right.hex)
-
-    } else {
-      display.lcd.setCursor(11, 2)
-      display.lcd.print(battery.warning.hex)
-    }
-  }
-
   async getTempValues(temperature) {
     try {
       let { celsius, fahrenheit } = temperature
@@ -248,7 +145,7 @@ class StationStats {
 
         display.lcd.setCursor(14, 1)
         display.lcd.print(`${temp.warning.hex}${temp.degree.hex}F`)
-        } else if (celsius == -100) {
+      } else if (celsius == -100) {
 
 
       } else {
