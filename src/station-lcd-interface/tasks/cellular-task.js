a912f7fcdc724f1e9f81429b7aea1c3b96ad1016
Meelyn Pandit <meelyn.pandit@gmail.com>
2024-11-18 15:45:30 +0000

    display cell signal as percent and rssi in dbm

--- src/station-lcd-interface/tasks/cellular-task.js
+++ src/station-lcd-interface/tasks/cellular-task.js
@@ -19,8 +19,10 @@ class CellularCarrier {
         .then(res => {
           let carrier = res.carrier
           let signal = res.signal.toString()
+          let rssi = res.rssi.toString()
 
-          resolve([this.header, carrier, `${signal}%`])
+          resolve([this.header, carrier, `${signal}%`, `${rssi} dBm`])
+
           // resolve([this.header, res.carrier, res.signal])
         })
         .catch(error => {
