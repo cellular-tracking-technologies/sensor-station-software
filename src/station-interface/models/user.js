bfdd0c64fa39cd8f830c97cd05e0cfaea68bb177
Meelyn Pandit <meelyn.pandit@gmail.com>
2025-05-20 00:09:37 +0000

    add sqlite3

--- src/station-interface/models/user.js
+++ src/station-interface/models/user.js
@@ -1,10 +1,29 @@
-import duckdb from '@duckdb/node-api';
-import bcrypt from "bcrypt";
+import sqlite3 from 'sqlite3'
+const db = new sqlite3.Database(':memory:');
 
-const db = await duckdb.DuckDBInstance.create('./my_duckdb.db');
-const connection = await db.connect();
+db.serialize(() => {
+  db.run("CREATE TABLE lorem (info TEXT)");
 
-connection.exec('CREATE SCHEMA IF NOT EXISTS user_schema')
-connection.exec('CREATE TABLE IF NOT EXISTS user_schema.users (email TEXT, password TEXT)')
+  const stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+  for (let i = 0; i < 10; i++) {
+    stmt.run("Ipsum " + i);
+  }
+  stmt.finalize();
 
-const result = connection.exec()
+  db.each("SELECT rowid AS id, info FROM lorem", (err, row) => {
+    console.log(row.id + ": " + row.info);
+  });
+});
+
+db.close();
+
+// import duckdb from '@duckdb/node-api';
+// import bcrypt from "bcrypt";
+
+// const db = await duckdb.DuckDBInstance.create('./my_duckdb.db');
+// const connection = await db.connect();
+
+// connection.exec('CREATE SCHEMA IF NOT EXISTS user_schema')
+// connection.exec('CREATE TABLE IF NOT EXISTS user_schema.users (email TEXT, password TEXT)')
+
+// const result = connection.exec()
