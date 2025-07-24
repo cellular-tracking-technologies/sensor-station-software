5129cbffb2eff0ce1a0bd133160b16cb72d59a65
Bob Fogg <Bob Fogg>
2025-07-24 12:31:50 -0400

    run initialize script during station update

--- system/scripts/update-station.sh
+++ system/scripts/update-station.sh
@@ -34,6 +34,9 @@ fi
 
 sudo sh -c "date -u +'%Y-%m-%d %H:%M:%S' > /etc/ctt/station-software"
 
+# run the initialize script to ensure station files are written from i2c chips
+/usr/local/bin/node /lib/ctt/sensor-station-software/src/hardware/initialize.js
+
 echo '*******************************************'
 echo 'CTT Sensor Station Software Update Complete'
 echo '*******************************************'
