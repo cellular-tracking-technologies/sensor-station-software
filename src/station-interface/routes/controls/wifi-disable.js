b6ed6964229dc6831acd9618d03f7bfdc88f0099
Bob Fogg <Bob Fogg>
2025-08-20 16:41:33 -0400

    web wifi control fix import after refactor

--- src/station-interface/routes/controls/wifi-disable.js
+++ src/station-interface/routes/controls/wifi-disable.js
@@ -1,3 +1,5 @@
+import RunCommand from '../../../command.js'
+
 export default async (req, res) => {
   await RunCommand('/bin/bash /lib/ctt/sensor-station-software/system/scripts/disable-wifi.sh')
   return res.status(200).send()
